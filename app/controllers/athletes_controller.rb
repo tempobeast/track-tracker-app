82b969ebd267be43146c67c47840599254123a6f
Zach <shaw.zacht@gmail.com>
2022-11-18 23:41:27 -0800

    create and show actions

--- app/controllers/athletes_controller.rb
+++ app/controllers/athletes_controller.rb
@@ -1,2 +1,12 @@
 class AthletesController < ApplicationController
+    def create 
+        athlete = Athlete.create!(user_params)
+        session[:user_id] = user.id
+        render json: [athlete], status: :created
+    end
+
+    def show
+        athlete = find_user
+        render json: athlete, status: :ok
+    end
 end
