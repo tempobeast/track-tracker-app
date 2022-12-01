e123bce4652ecb51e23dfe172947d642de5cc252
Zach <shaw.zacht@gmail.com>
2022-11-30 22:27:48 -0800

    authorize user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,15 @@ class ApplicationController < ActionController::API
     include ActionController::Cookies
 
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-    rescue_from ActionRecord::RecordInvalid, with: :render_unprocessable_entity
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+
+    before_action :authorize
 
     private
+
+    def authorize
+        return render json: { errors: ["Not Authorized"]}, status: :unauthorized unless session.include? :user_id
+    end
 
     def find_user
         user.find_by(id: session[:user_id])
