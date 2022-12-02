025c317228dcd2caf6142c842999c64b834cd953
Zach <shaw.zacht@gmail.com>
2022-12-01 21:48:53 -0800

    view all teams

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,10 @@
 class TeamsController < ApplicationController
+
+    skip_before_action :authorize, except: [:destroy, :update]
+
+    def index
+        teams = Team.all
+        render json: teams, status: :ok
+    end
+
 end
