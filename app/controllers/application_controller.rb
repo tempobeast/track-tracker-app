b8464b41898473aa53f6ffcbfee0c95c1ff17a30
Zach <shaw.zacht@gmail.com>
2022-12-05 21:57:44 -0800

    fixed typo onfind_user action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::API
     end
 
     def find_user
-        user.find_by(id: session[:user_id])
+        User.find_by(id: session[:user_id])
     end
 
     def render_unprocessable_entity(invalid)
