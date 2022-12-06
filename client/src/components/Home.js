b19d2ae1c8bf273f236583254207fae270dfb628
Zach <shaw.zacht@gmail.com>
2022-12-05 22:17:57 -0800

    init home

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+
+function Home() {
+
+    return (
+      <div id="home">
+        <h2>Welcome to Track-it!</h2>
+        <h3>Sign Up / Login to plan and track your running progress</h3>
+      </div>
+    );
+  }
+export default Home;
