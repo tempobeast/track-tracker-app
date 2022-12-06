da590aa1006c39c262cc2870c700579a5a81bad6
Zach <shaw.zacht@gmail.com>
2022-12-05 22:17:27 -0800

    init header

---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+
+function Header() {
+
+return (
+    <div id="header">
+        <h1>Track-It!</h1>
+    </div>
+    
+  );
+}
+export default Header
