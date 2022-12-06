4fef4e6900c04ee0c3edb2807ee0ea3210e75856
Zach <shaw.zacht@gmail.com>
2022-12-05 22:06:13 -0800

    view athlete team_id

--- app/serializers/athlete_serializer.rb
+++ app/serializers/athlete_serializer.rb
@@ -1,3 +1,3 @@
 class AthleteSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :username, :password_digest, :age, :team_id
+  attributes :id, :first_name, :last_name, :username, :password_digest, :team_id
 end
