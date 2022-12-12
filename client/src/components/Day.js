18f36abd6ee6d92b1f774f8fc0719cc0d95d56de
Zach <shaw.zacht@gmail.com>
2022-12-12 15:31:19 -0800

    removed Day component

--- client/src/components/Day.js
+++ client/src/components/Day.js
@@ -3,13 +3,13 @@ import "../App.css";
 
 function Day({value}) {
 
-    const shortDate = value.toString().split("").slice(0, 15)
-    
+    const shortDate = value.toString().split("").slice(0, 15).join("")
+
+    let date = new Date()
 
 return (
     <div id="header">
         <h1>{shortDate}</h1>
-        
     </div>
     
   );
