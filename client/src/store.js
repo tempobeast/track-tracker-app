7b7f32f58280a744887619b9961a61ac63a9564e
Zach <shaw.zacht@gmail.com>
2022-12-11 22:39:05 -0800

    store workouts

--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from '@reduxjs/toolkit'
 import userReducer from './features/user/userSlice'
 import teamsReducer from './features/teams/teamSlice'
+import workoutsReducer from './features/workouts/workoutsSlice'
 
 export default configureStore({
     reducer: {
         user: userReducer,
         teams: teamsReducer,
+        workouts: workoutsReducer,
     }
 })
