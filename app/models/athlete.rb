aead133dfbe7a49ecefd34cf729cd29779858d76
Zach <shaw.zacht@gmail.com>
2022-12-11 22:55:38 -0800

    associate athlete with one coach

--- app/models/athlete.rb
+++ app/models/athlete.rb
@@ -1,8 +1,8 @@
 class Athlete < User
 
-    has_one :team
+    belongs_to :team
     has_many :log_entries
     has_many :workouts, through: :log_entries
-    has_many :workout_groups
-    has_many :coaches, through: :workout_groups
+    has_one :workout_group
+    has_one :coach, through: :workout_groups
 end
