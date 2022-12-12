e030cffe8cf830b95850969601cfb2f7fe4941dc
Zach <shaw.zacht@gmail.com>
2022-12-11 22:56:56 -0800

    associate coach with workouts

--- app/serializers/coach_serializer.rb
+++ app/serializers/coach_serializer.rb
@@ -1,3 +1,7 @@
 class CoachSerializer < ActiveModel::Serializer
-  attributes :id, :first_name, :last_name, :username, :password_digest, :team_id
+  attributes :id, :first_name, :last_name, :username, :password_digest, :team_id, :workout_groups, :athletes
+
+  belongs_to :workout_groups
+  has_many :athletes, through: :workout_groups
+  has_many :workouts
 end
