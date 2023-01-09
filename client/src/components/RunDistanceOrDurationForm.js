a467941b958fe2e13220e576dece3796d1361b78
Zach <shaw.zacht@gmail.com>
2023-01-09 13:00:41 -0800

    abstract state to CreateWorkout component

--- client/src/components/RunDistanceOrDurationForm.js
+++ client/src/components/RunDistanceOrDurationForm.js
@@ -1,36 +1,19 @@
-import React, {useState} from "react";
+import React from "react";
 
-function RunDistanceOrDurationForm( {newWorkoutFormData, setNewWorkoutFormData} ) {
+function RunDistanceOrDurationForm( {handleFormChange} ) {
 
-    const [runDetail, setRunDetail] = useState({
-        distance_or_duration: "",
-        unit_of_measure: "meters"
-    })
-
-    function handleRunDetailChange(e) {
-        setRunDetail({...runDetail, [e.target.name]: e.target.value})
-    }
-
-    function handleRunDetailSubmit(e) {
-        e.preventDefault();
-        const detail = `${runDetail.distance_or_duration} ${runDetail.unit_of_measure} run`
-        setNewWorkoutFormData({...newWorkoutFormData, details: detail})
-    }
 
     return (
         <div>
-            <form onSubmit={handleRunDetailSubmit}>
                 <div className="interval_form_item">
                     <label>Description: </label>
-                    <input onChange={handleRunDetailChange} name="distance_or_duration" type="number"></input>
-                    <select name="unit_of_measure" onChange={handleRunDetailChange}>
-                        <option defaultValue="meters">meters</option>
-                        <option value="mile">miles</option>
+                    <input onChange={handleFormChange} name="distance_or_duration" type="number"></input>
+                    <select name="unit_of_measure" onChange={handleFormChange}>
+                        <option defaultValue="miles">miles</option>
+                        <option value="meters">meters</option>
                         <option value="minute">minutes</option>
                     </select>
                 </div>
-                <button type="submit">Next</button>
-            </form>
         </div>
     )
 }
