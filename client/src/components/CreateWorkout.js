50f2cfed423927275a056a048e242822a326516a
Zach <shaw.zacht@gmail.com>
2023-01-24 15:47:26 -0800

    render form new workout form based on workoutType

--- client/src/components/CreateWorkout.js
+++ client/src/components/CreateWorkout.js
@@ -1,65 +1,19 @@
 import React, { useState } from "react";
 import "../App.css";
-import {useSelector, useDispatch} from 'react-redux'
-import { setWorkouts } from '../features/workouts/workoutsSlice'
-import IntervalForm from "./IntervalForm";
+import IntervalItemForm from "./IntervalItemForm";
 import RunDistanceOrDurationForm from "./RunDistanceOrDurationForm";
 
 function CreateNewWorkout ({ dateToString, dateToISO }) {
 
-    const dispatch = useDispatch()
-    // const user = useSelector((state) => state.user.value)
-    const workouts = useSelector((state) => state.workouts.value)
-    const [errors, setErrors] = useState([])
-
-    const [newWorkoutFormData, setNewWorkoutFormData] = useState({
-        date: dateToISO,
-        workout_type: "Long Run",
-        distance_or_duration: "",
-        unit_of_measure: "miles",
-        add_ons: ""
-    })
-
-    function handleFormChange(e) {
-        setNewWorkoutFormData({...newWorkoutFormData,
-            [e.target.name]: e.target.value
-        })
-    }
-
-    function handleFormSubmit(e){
-        e.preventDefault();
-        const runDetails = `${newWorkoutFormData.distance_or_duration} ${newWorkoutFormData.unit_of_measure} run`
-        const formDataToSubmit = {
-            date: dateToISO,
-            workout_type: newWorkoutFormData.workout_type,
-            details: runDetails,
-            add_ons: newWorkoutFormData.add_ons
-        }
-        fetch("/workouts", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(formDataToSubmit),
-        }).then((res) => {
-            if (res.ok) {
-                res.json().then((workout) => {
-                    dispatch(setWorkouts([...workouts, workout]))
-                }
-                )
-            } else {
-                res.json().then((err) => setErrors(err.errors))
-            }
-        })
-    }
+    const [ workoutType, setWorkoutType ] = useState("Long Run")
 
     return (
         <div>
         <h2>{dateToString}</h2>
-            <form onSubmit={handleFormSubmit} className="new_workout_form">
+            <form className="new_workout_form">
                 <label htmlFor="workout_type">
                     workout type: 
-                    <select name="workout_type" onChange={handleFormChange} value={newWorkoutFormData.workout_type}>
+                    <select name="workout_type" onChange={(e) => setWorkoutType(e.target.value)} value={workoutType}>
                         <option value="Long Run">Long Run</option>
                         <option value="Interval">Interval</option>
                         <option value="Recovery">Recovery</option>
@@ -68,21 +22,15 @@ function CreateNewWorkout ({ dateToString, dateToISO }) {
                         <option value="Rest">Rest</option>
                     </select>
                 </label><br/>
-                { 
-                    newWorkoutFormData.workout_type === "Long Run" || newWorkoutFormData.workout_type === "Recovery" 
-                    ? <RunDistanceOrDurationForm 
-                        // onDetailSubmit={onDetailSubmit}
-                        handleFormChange={handleFormChange}
-                        />
-                    : newWorkoutFormData.workout_type === "Rest" 
-                    ? null 
-                    :  <IntervalForm/>
-                }
-                <button onSubmit={handleFormSubmit}>Submit Workout</button>
-                {errors ? <p>{errors}</p> : null}
             </form>
-        </div>
-        
+            { 
+                workoutType === "Long Run" || workoutType === "Recovery" 
+                ? <RunDistanceOrDurationForm dateToString={dateToString} workoutType={workoutType}/>
+                : workoutType === "Interval" || workoutType === "Tempo"
+                ? <IntervalItemForm dateToString={dateToString} workoutType={workoutType}/>
+                :  <p>Race or Rest placeholder</p>
+            }
+        </div>  
     )
 }
 
