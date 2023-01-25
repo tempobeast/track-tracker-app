1d2b7ae499e7b4fefb157aaff593bd986d63721e
Zach <shaw.zacht@gmail.com>
2023-01-24 16:00:49 -0800

    handle array of details rather than string

--- client/src/components/WorkoutCard.js
+++ client/src/components/WorkoutCard.js
@@ -3,7 +3,7 @@ import "../App.css";
 
 function WorkoutCard ({ workout, dateToString }) {
 
-    const {workout_type, details, approx_duration, add_ons} = workout
+    const {workout_type, details, add_ons} = workout
     const createId = () => workout_type.toLowerCase().split(" ").join("_")
     const dateSplit = dateToString.split(" ")
     const day = dateSplit[0]
@@ -11,6 +11,9 @@ function WorkoutCard ({ workout, dateToString }) {
     const dateNumber = dateSplit[2]
     const year = dateSplit[3]
 
+    const detailsToDisplay = details.map((detail) => <p key={detail}>{detail}</p>)
+
+    
     // const displayWorkoutDetails = details.map((detail) => <p>{detail}</p>)
     
     // const averageWorkoutRating = workout.log_entries.reduce(function(acc, entry) {return acc + entry.workout_rating}, 0)/workout.log_entries.length;
@@ -22,7 +25,7 @@ function WorkoutCard ({ workout, dateToString }) {
                 <h4 className="workout_month">{month.toUpperCase()}</h4>
             </div>
             <h2 className="workout_type">{workout_type}</h2>
-            {workout_type === "Long Run" ? <p>{details}</p> : details.map((detail) => <p>{detail}</p>)}
+            {detailsToDisplay}
             <h3 className="workout_year">{year}</h3>
         </div>
     )
