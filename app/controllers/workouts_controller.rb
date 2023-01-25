4bbe0bc9c380eec5fa6404486c5e6d03aee8bc8a
Zach <shaw.zacht@gmail.com>
2023-01-24 16:02:09 -0800

    receive array of details

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,2 +1,30 @@
 class WorkoutsController < ApplicationController
+
+    def create
+        user = find_user
+        if user
+            workout = user.workouts.create!(workout_params)
+            render json: workout, status: :created
+        else
+            render json: {errors: ["Unauthorized"]}, status: :unauthorized
+        end
+    end
+
+    def index
+        user = find_user
+        if user
+            workouts = user.workouts
+            render json: workouts, status: :ok
+        else
+            render json: {errors: ["Unauthorized"]}, status: :unauthorized
+        end
+    end
+
+
+
+    private
+
+    def workout_params
+        params.permit(:date, :workout_type, :add_ons, :details => [])
+    end
 end
