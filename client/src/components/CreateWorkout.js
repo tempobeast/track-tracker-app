8d239ff88d630327deb361fc8710ec3a8497c405
Zach <shaw.zacht@gmail.com>
2023-02-04 23:23:24 -0800

    add rest day with no run details

--- client/src/components/CreateWorkout.js
+++ client/src/components/CreateWorkout.js
@@ -2,10 +2,42 @@ import React, { useState } from "react";
 import "../App.css";
 import IntervalItemForm from "./IntervalItemForm";
 import RunDistanceOrDurationForm from "./RunDistanceOrDurationForm";
+import { setWorkouts } from '../features/workouts/workoutsSlice'
+import {useSelector, useDispatch} from 'react-redux'
 
-function CreateNewWorkout ({ dateToString, dateToISO }) {
+
+
+function CreateNewWorkout ({ dateToString }) {
 
     const [ workoutType, setWorkoutType ] = useState("Long Run")
+    const dispatch = useDispatch()
+    const workouts = useSelector((state) => state.workouts.value)
+    const [errors, setErrors] = useState([])
+
+
+    function handleRestSubmit(e){
+        const formDataToSubmit = {
+            date: dateToString,
+            workout_type: workoutType,
+            details: ["Take the day off"],
+        }
+        fetch("/workouts", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formDataToSubmit),
+        }).then((res) => {
+            if (res.ok) {
+                res.json().then((workout) => {
+                    dispatch(setWorkouts([...workouts, workout]))
+                }
+                )
+            } else {
+                res.json().then((err) => setErrors(err.errors))
+            }
+        })
+    }
 
     return (
         <div>
@@ -28,7 +60,9 @@ function CreateNewWorkout ({ dateToString, dateToISO }) {
                 ? <RunDistanceOrDurationForm dateToString={dateToString} workoutType={workoutType}/>
                 : workoutType === "Interval" || workoutType === "Tempo"
                 ? <IntervalItemForm dateToString={dateToString} workoutType={workoutType}/>
-                :  <p>Race or Rest placeholder</p>
+                :  workoutType === "Rest" 
+                ? <button onClick={handleRestSubmit}>Submit Rest Day</button>
+                : <p>Race placeholder</p>
             }
         </div>  
     )
