b4bf816432e4801e18636bec6c8aaf9e533dd40a
Zach <shaw.zacht@gmail.com>
2023-04-16 13:41:52 -0700

    logout in Navbar

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,13 +1,30 @@
 import React from "react";
 import "../App.css";
 import { NavLink } from "react-router-dom"
+import {setUser} from '../features/user/userSlice'
+import {useSelector, useDispatch} from 'react-redux'
+import { useNavigate } from "react-router-dom"
 
 function Nav() {
+
+  const user = useSelector((state) => state.user.value);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  function handleLogoutClick(e) {
+    fetch(`/logout`, { method: "DELETE" }).then((res) => {
+      if (res.ok) {
+        dispatch(setUser(null))
+        navigate("/")
+      }
+    });
+  }
 
 return (
     <div id="nav">
       <NavLink className="nav_button" to="/user_profile">Home</NavLink>
       <NavLink className="nav_button" to="/view_athletes">View Athletes</NavLink>
+      {user ? <button onClick={handleLogoutClick}>logout</button> : null}
     </div>
     
   );
