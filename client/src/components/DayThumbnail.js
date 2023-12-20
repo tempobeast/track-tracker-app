f88fdbcbcd50a5a1a7ed302a265c5a98cfbf82ca
Zach <shaw.zacht@gmail.com>
2023-12-19 22:30:35 -0800

    mobile first styling for athlete workout calendar

--- client/src/components/DayThumbnail.js
+++ client/src/components/DayThumbnail.js
@@ -17,7 +17,10 @@ function DayThumbnail ({day, onCalendarChange}) {
     
     const workout = workouts.find((workout) => workout.date === format(day, "yyyy-LL-dd"))
     const createId = () => workout.workout_type.toLowerCase().split(" ").join("_")
-    
+    console.log(workout)
+    const detailsToDisplay = workout ? workout.details.map((detail) => <p key={detail}>{detail}</p>) : []
+
+
     return (
         <div className="day_thumb" id={workout ? createId() : "no_workout"} onClick={handleThumbnailClick}>
             <div className="day_thumb_date_container">
@@ -25,6 +28,18 @@ function DayThumbnail ({day, onCalendarChange}) {
                 <h4 className="day_thumb_month">{month.toUpperCase()}</h4>
             </div>
             {workout ? <h5 className="day_thumb_detail">{workout.workout_type}</h5> : <h5 className="day_thumb_detail">Add Workout</h5>}
+    
+        {workout ? 
+        <div className="workout_card day_thumb-mobile" id={workout ? createId() : "no_workout"}>
+            <div className="workout_card_header">
+                <h1 className="workout_date">{dateNumber}</h1>
+                <h4 className="workout_month">{month.toUpperCase()}</h4>
+            </div>
+            <h2 className="workout_type">{workout.workout_type}</h2>
+            {detailsToDisplay}
+            <h3 className="workout_year">{workout.year}</h3>
+        </div>
+        : null}
         </div>
     )
 }
